a85a547dab0489adeda08b7bbf38aaadb48e11b7
kosuke.obata <kosuke.obata@bizreach.co.jp>
2016-10-11 20:15:56 +0900

    Change root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :introductions
+  root to: redirect('/introductions')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
